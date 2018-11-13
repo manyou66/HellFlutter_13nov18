7ef26fc4a56014f110ffdeb078ad91edd9a71586
browniApp <wanpracha.n@rmutsv.ac.th>
2018-11-13 11:30:20 +0700

    Standart Flutter

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //make pages on mobile phone
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Hello Bow',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Hello bow appbar'),
+        ),
+        body: new Text('This is Body'),
+      ),
+    );
+  }
+}
